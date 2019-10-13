e0079674a8cc3baa2c1f2b7be98e1f9a53714e3d
fishingguy456 <43834326+fishingguy456@users.noreply.github.com>
2019-10-13 10:01:57 -0400

    Update Fire.js

--- OpenLearn_React/src/config/Fire.js
+++ OpenLearn_React/src/config/Fire.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyDUSBweUIWfTGfNBS9d0c3yYxt8OAZqe5w",
-  authDomain: "login-b92c7.firebaseapp.com",
-  databaseURL: "https://login-b92c7.firebaseio.com",
-  projectId: "login-b92c7",
-  storageBucket: "565329838790",
-  messagingSenderId: "565329838790"
+  apiKey: "itsasecret",
+  authDomain: "secretidk.firebaseapp.com",
+  databaseURL: "https://nobodyknows.firebaseio.com",
+  projectId: "jkweknow",
+  storageBucket: "PGMAKINBUCKETS",
+  messagingSenderId: "idkwhattoputanymore"
 };
 
 const fire = firebase.initializeApp(config);
